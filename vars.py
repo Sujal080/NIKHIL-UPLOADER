d1f8ec705f3ddc460cba8cea2cc245b1ea1da7a8
sujalchavdaa <sujalchavda500@gmail.com>
2025-06-19 14:52:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", "25933223"))
 API_HASH = environ.get("API_HASH", "6ef5a426d85b7f01562a41e6416791d3")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7373062125:AAFJY69INZUEF3tcnQ4ORohcOxb3zi4QMUA")
 OWNER = int(environ.get("OWNER", "8118667253"))
-CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+CREDIT = "𝓢𝓤𝓙𝓐𝓛 𝘽𝙊𝙏𝙎"
 AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
